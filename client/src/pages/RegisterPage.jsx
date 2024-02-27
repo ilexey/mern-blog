917c86ccc3e7e90ace7f13944fabb3ea6d61fddb
ilexey <alexeyshitenkov@yandex.ru>
2024-02-27 15:51:41 +0300

    Updates

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+    <form
+      className="w-1/4 h-60 mx-auto mt-40"
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <h1 className="text-lg text-white text-center">
+        Регистрация
+      </h1>
+      <label className="text-xs text-gray-400">
+        Имя пользователя:
+        <input
+          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-nome placeholder:text-gray-700"
+          type="text"
+          placeholder="Имя пользователя"
+        />
+      </label>
+      <label className="text-xs text-gray-400">
+        Пароль:
+        <input
+          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-nome placeholder:text-gray-700"
+          type="password"
+          placeholder="Пароль"
+        />
+      </label>
+
+      <div className="flex gap-8 justify-center mt-4">
+        <button
+          type="submit"
+          className="flex justify-center items-center text-xs bg-gray-600 text-white rounded-sm py-2 px-4"
+        >
+          Подтвердить
+        </button>
+        <Link
+          to="/login"
+          className="flex justify-center items-center text-xs text-white"
+        >
+          Уже зарегистрированы?
+        </Link>
+      </div>
+    </form>
   )
 }
 
